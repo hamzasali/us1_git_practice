557d4c76b5813dcddf2051155af35dddf88e6aba
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-09 11:31:10 -0400

    HelloWorld.java is updated

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,6 +3,8 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
 
+        System.out.println("Hello World");
+
         System.out.println("Good Old Days");
     }
 }
